c3d69b5ff0825121a18fb5db943b73f0df6744d8
Robert Fiorentino <RobertFiorentino@users.noreply.github.com>
2025-12-15 14:18:41 -0500

    Update .eleventy.js for image alignment

--- .eleventy.js
+++ .eleventy.js
@@ -527,6 +527,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
+  eleventyConfig.addPassthroughCopy({
+    "src/site/z_Assets": "z_Assets"
+  });
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
